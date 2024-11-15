f1a56da64b250d1bbbf402f09b48df05917116b2
Am-Am-Am <amur_almukhametov@mail.ru>
2024-11-15 13:44:59 +0500

    change port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT ?? 4200);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
